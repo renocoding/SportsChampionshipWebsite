af512b729648a13d281eb33c26bd932344a90db9
Antonio Domel <antoniodomel@antonios-mbp-2.dhcp.nd.edu>
2020-10-18 14:38:09 -0400

    Updated champ_library and started yearController

--- Server/champ_library.py
+++ Server/champ_library.py
@@ -8,7 +8,29 @@ class champ_database:
         self.city_data = dict()
 
 
-    def load_data(self, filename):
+    def load_city_data(self, filename):
+
+        # json parsing in python, source used - https://www.geeksforgeeks.org/read-json-file-using-python/
+        json_file = open("data_by_year_start1940.json");
+        json_data = json.load(json_file)
+
+        # loop through each year
+        for year, result in json_data.items():
+
+            # loop through each sport in each year
+            for sport in result:
+
+                # get data for each sport and update our city-sorted data
+                if sport['sport'] == "NBA" or sport['sport'] == "MLB" or sport['sport'] == "NFL" or sport['sport'] == "NHL":
+                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
+                elif sport['level'] == "college" and sport['sport'] == "Basketball (M)":
+                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
+                elif sport['level'] == "college" and sport['sport'] == "Basketball (W)":
+                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
+                elif sport['level'] == "college" and sport['sport'] == "Football (M)":
+                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
+
+    def load_year_data(self, filename):
 
         # json parsing in python, source used - https://www.geeksforgeeks.org/read-json-file-using-python/
         json_file = open("data_by_year_start1940.json");
@@ -21,19 +43,16 @@ class champ_database:
             # loop through each sport in each year
             for sport in result:
 
-                # get data for each sport and update our year-sorted and city-sorted data
+                # get data for each sport and update our year-sorted
                 if sport['sport'] == "NBA" or sport['sport'] == "MLB" or sport['sport'] == "NFL" or sport['sport'] == "NHL":
                    self.year_data[year][sport['sport']] = sport['winner']
-                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
                 elif sport['level'] == "college" and sport['sport'] == "Basketball (M)":
                    self.year_data[year]["NCAA Basketball (M)"] = sport['winner']
-                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
                 elif sport['level'] == "college" and sport['sport'] == "Basketball (W)":
                    self.year_data[year]["NCAA Basketball (W)"] = sport['winner']
-                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
                 elif sport['level'] == "college" and sport['sport'] == "Football (M)":
                    self.year_data[year]["NCAA Football (M)"] = sport['winner']
-                   self.city_data[sport['winner_metro']] = 1 + self.city_data.get(sport['winner_metro'], 0)
+
 
     def get_year(self, year):
 
@@ -76,5 +95,6 @@ if __name__ == "__main__":
     # create champ database object
     cdb = champ_database()
 
-    # fill to dictionaries
-    cdb.load_data("data_by_year_start1940.json")
+    # fill two dictionaries
+    cdb.load_year_data("data_by_year_start1940.json")
+    cdb.load_city_data("data_by_year_start1940.json")
--- Server/yearController.py
+++ Server/yearController.py
@@ -1,1 +1,51 @@
-test
+import cherrypy
+import re, json
+from champ_library import champ_database
+
+class YearController(object):
+
+        def __init__(self, cdb=None):
+                if cdb is None:
+                        self.cdb = champ_database()
+                else:
+                        self.cdb = cdb
+
+                self.cdb.load_year_data('data_by_year_start1940.json')
+
+        def GET_KEY(self, year):
+		'''when GET request for /years/year comes in, then we respond with json string'''
+                output = {'result':'success'}
+
+                try:
+
+                        # returns dict of form {'MLB': 'White Sox', 'NBA' : 'Bulls'....}
+                        winners_in_year = self.cdb.get_year(year)
+
+                        # year exists in cdb
+                        if winners_in_year is not None:
+                            output['year'] = year
+                            output['championship_data'] = winners_in_year
+
+                        # year does not exist
+                        else:
+                            output['result'] = 'error'
+                            output['message'] = 'movie not found'
+
+                # catch other errors
+                except Exception as ex:
+                        output['result'] = 'error'
+                        output['message'] = str(ex)
+
+                return json.dumps(output)
+
+        def PUT_KEY(self, year):
+		'''when PUT request for /years/year comes in, then we change that year in the cdb'''
+                output = {'result':'success'}
+
+                # get body from put request
+                winners_in_year = json.loads(cherrypy.request.body.read().decode('utf-8'))
+
+                # call helper function to make data of the form {year: {'MLB': 'White Sox', 'NBA' : 'Bulls'....}}
+                self.cdb.set_year(year, winners_in_year)
+
+                return json.dumps(output)
